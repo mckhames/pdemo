bc6e7ba16c1bcaa68e09f54688ee9d077a11e9f6
McKenzie <mckhames@gmail.com>
2020-10-20 11:06:31 -0400

    Added material-ui/core

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/index.css'
+import '@material-ui/core'
+import '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
